cd4ab0011fdff811db3f7d7502de155d8d76d891
Toufiq Talukder <toufiq822553@gmail.com>
2026-09-24 01:01:10 +0600

    fix supabase database connection

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.ikusbspirsydtjjwcljl:fastapihousingandroommate@aws-0-ap-northeast-2.pooler.supabase.com:5432/postgres'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.ikusbspirsydtjjwcljl:housingandroommatemanagementproject@aws-0-ap-northeast-2.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
