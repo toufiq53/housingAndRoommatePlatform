45eb09dd50de4abb41283fbc9a633481d3c498e0
Toufiq Talukder <toufiq822553@gmail.com>
2026-09-24 00:38:40 +0600

    fix postgresql connection

--- database.py
+++ database.py
@@ -4,8 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./housing.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
 Base = declarative_base()
