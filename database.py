566ebb91a2cc5e7e54885407ccbd3a7d56476796
Toufiq Talukder <toufiq822553@gmail.com>
2026-09-24 00:43:17 +0600

    postgresql added

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./housing.db'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres.ikusbspirsydtjjwcljl:fastapihousingandroommate@aws-0-ap-northeast-2.pooler.supabase.com:5432/postgres'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
